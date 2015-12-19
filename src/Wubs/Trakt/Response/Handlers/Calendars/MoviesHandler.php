b5fe1e09e8d7183d559578845a27e8544f7241a4
Valentin GOT <gotvalentin@gmail.com>
2015-12-19 19:55:05 +0000

    fix(MoviesHandler): adapt for GuzzleHttp 6.1

--- src/Wubs/Trakt/Response/Handlers/Calendars/MoviesHandler.php
+++ src/Wubs/Trakt/Response/Handlers/Calendars/MoviesHandler.php
@@ -1,33 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bwubs
- * Date: 12/03/15
- * Time: 12:39
- */
 
 namespace Wubs\Trakt\Response\Handlers\Calendars;
 
-
-use GuzzleHttp\Message\ResponseInterface;
+use GuzzleHttp\ClientInterface;
+use Psr\Http\Message\ResponseInterface;
 use Wubs\Trakt\Contracts\ResponseHandler;
 use Wubs\Trakt\Request\Parameters\Days;
 use Wubs\Trakt\Request\Parameters\Type;
 use Wubs\Trakt\Response\Calendar\Calendar;
-use Wubs\Trakt\Response\Calendar\Day;
 use Wubs\Trakt\Response\Handlers\AbstractResponseHandler;
-use Wubs\Trakt\Response\Handlers\DefaultResponseHandler;
 
-class MoviesHandler extends AbstractResponseHandler implements ResponseHandler
-{
+class MoviesHandler extends AbstractResponseHandler implements ResponseHandler {
 
     /**
      * @param ResponseInterface $response
-     * @param \GuzzleHttp\ClientInterface|GuzzleHttp\ClientInterface $client
+     * @param ClientInterface $client
      * @return Calendar
      */
-    public function handle(ResponseInterface $response, \GuzzleHttp\ClientInterface $client)
-    {
+    public function handle(ResponseInterface $response, ClientInterface $client) {
         $json = $this->getJson($response);
 
         return new Calendar($json, Type::movie(), $this->getClientId(), $this->getToken(), $client);
