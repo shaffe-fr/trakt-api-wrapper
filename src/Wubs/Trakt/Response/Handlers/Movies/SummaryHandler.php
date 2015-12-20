afe81e1725f7f976b9b251a96ffd88e0ce264309
Valentin GOT <gotvalentin@gmail.com>
2015-12-20 12:10:08 +0000

    fix(SummaryHandler): adapt for GuzzleHttp 6.1

--- src/Wubs/Trakt/Response/Handlers/Movies/SummaryHandler.php
+++ src/Wubs/Trakt/Response/Handlers/Movies/SummaryHandler.php
@@ -1,24 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bwubs
- * Date: 18/03/15
- * Time: 14:59
- */
 
 namespace Wubs\Trakt\Response\Handlers\Movies;
 
-
-use GuzzleHttp\Message\ResponseInterface;
+use GuzzleHttp\ClientInterface;
+use Psr\Http\Message\ResponseInterface;
 use Wubs\Trakt\Contracts\ResponseHandler;
 use Wubs\Trakt\Media\Movie;
 use Wubs\Trakt\Response\Handlers\AbstractResponseHandler;
 
-class SummaryHandler extends AbstractResponseHandler implements ResponseHandler
-{
+class SummaryHandler extends AbstractResponseHandler implements ResponseHandler {
 
-    public function handle(ResponseInterface $response, \GuzzleHttp\ClientInterface $client)
-    {
+    public function handle(ResponseInterface $response, ClientInterface $client) {
         $json = $this->getJson($response);
 
         return new Movie($json, $this->getClientId(), $this->getToken(), $client);
