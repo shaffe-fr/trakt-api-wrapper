64ee9fbdc1feebe3637bc9684514a8405a4aeb82
daj92109 <daj92109@yahoo.com>
2016-10-29 10:48:04 +1100

    Updated ResponseInterface.

--- src/Wubs/Trakt/Response/Handlers/Search/SearchHandler.php
+++ src/Wubs/Trakt/Response/Handlers/Search/SearchHandler.php
@@ -10,7 +10,7 @@ namespace Wubs\Trakt\Response\Handlers\Search;
 
 
 use GuzzleHttp\ClientInterface;
-use GuzzleHttp\Message\ResponseInterface;
+use Psr\Http\Message\ResponseInterface;
 use Illuminate\Support\Collection;
 use Wubs\Trakt\Exception\MediaTypeNotSupportedException;
 use Wubs\Trakt\Media\Episode;
