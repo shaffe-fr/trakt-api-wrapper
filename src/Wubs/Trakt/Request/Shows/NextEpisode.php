6565366c8e90f72b630bed618af1d6033f9176cf
AlienFX <eurl@alienfx.net>
2016-12-19 17:29:28 +0800

    Api NextEpisode

---
+++ src/Wubs/Trakt/Request/Shows/NextEpisode.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bwubs
+ * Date: 18/03/15
+ * Time: 14:56
+ */
+
+namespace Wubs\Trakt\Request\Shows;
+
+use Wubs\Trakt\Request\AbstractRequest;
+use Wubs\Trakt\Request\Parameters\MediaId;
+use Wubs\Trakt\Request\Parameters\MediaIdTrait;
+use Wubs\Trakt\Request\RequestType;
+use Wubs\Trakt\Response\Handlers\Movies\SummaryHandler;
+
+class NextEpisode extends AbstractRequest
+{
+    use MediaIdTrait;
+
+
+    /**
+     * @param int $mediaId
+     */
+    public function __construct($mediaId)
+    {
+        parent::__construct();
+        $this->id = $mediaId;
+//        $this->setResponseHandler(new SummaryHandler());
+    }
+
+    public function getRequestType()
+    {
+        return RequestType::GET;
+    }
+
+    public function getUri()
+    {
+        return "shows/:id/next_episode";
+    }
+}
